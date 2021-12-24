2400700c9626a7c4466ef03a870a76dd33b9dad0
pvictorf <pvictorferreira@hotmail.com>
2021-12-24 11:34:11 -0300

    make it better

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,22 +13,16 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['normalize.css/normalize.css', '@/assets/scss/base.scss'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/accessor'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: '@/components', pathPrefix: false }],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/style-resources',
     '@nuxtjs/axios',
